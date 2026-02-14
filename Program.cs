ed75def001bf822af1af5702852e1477385b06e2
Daniel Ikechukwu <Ikechukwudaniel080@gmail.com>
2026-02-14 10:41:33 +0100

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿namespace LambdaGenericDelegate {
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Func<int, float, double, double> obj1 = (x, y, z) =>
+            {
+                return x + y + z;
+            };
+
+            double result = obj1.Invoke(100, 125.45f, 456.789);
+
+            Console.WriteLine(result);
+        }
+    }
+}
