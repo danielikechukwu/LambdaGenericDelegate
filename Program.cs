2c7c001db1ba60c0b344f6f2e21922ba42efe0ce
Daniel Ikechukwu <Ikechukwudaniel080@gmail.com>
2026-02-14 10:51:11 +0100

    Feat(lambda): Implement Action generic delegate with lambda expression.

--- Program.cs
+++ Program.cs
@@ -11,6 +11,25 @@
             double result = obj1.Invoke(100, 125.45f, 456.789);
 
             Console.WriteLine(result);
+
+            Action<int, float, double> obj2 = (x, y, z) => {
+                Console.WriteLine(x + y + z);
+            };
+
+            obj2.Invoke(50, 255.45f, 123.456);
+
+            Predicate<string> obj3 = new Predicate<string>(CheckLength);
+
+            bool res = obj3.Invoke("Ikechukwu");
+
+            Console.WriteLine(res);
+        }
+
+        public static bool CheckLength(string name)
+        {
+            if (name.Length > 5)
+                return true;
+            return false;
         }
     }
 }
